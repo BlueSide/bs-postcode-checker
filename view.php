8b1f993b0b9e0196db9dbb9bf09c15eb3fec69e1
trajectcontrole <thomas@blueside.nl>
2018-03-01 16:42:36 +0100

    Changed classes, removed H1.

--- view.php
+++ view.php
@@ -1,10 +1,9 @@
-<div class="test">
-    <h1>Postcode Checker</h1>
+<div class="bs-postcode-form">
     <div id="pc-error">Geen geldige postcode!</div>
     <div id="pc-not-found">De postcode is niet gevonden!</div>
     <div id="pc-found">De postcode is niet gevonden!</div>
     <!-- <form> -->
-         <input type="text" id="postcode" />
+         <input type="text" id="postcode" class="bs-postcode-input"/>
          <button type="button" class="check-button">Check!</button>
     <!-- </form> -->
 </div>

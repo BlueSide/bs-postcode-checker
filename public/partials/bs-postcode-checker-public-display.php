eb4bfd7a0027bdbb263e3008400d47ce04ed929e
trajectcontrole <thomas@blueside.nl>
2018-04-17 15:19:18 +0200

    Added spinner

--- public/partials/bs-postcode-checker-public-display.php
+++ public/partials/bs-postcode-checker-public-display.php
@@ -16,12 +16,15 @@
     <div id="pc-error" style="display:none">Geen geldige postcode!</div>
     <div id="pc-not-found" style="display:none">Uw postcode is niet aangesloten aan ons warmtenet. </div>
     <div id="pc-found" style="display:none"></div>
-    <form class="uk-grid-small">
-            <div class="uk-width-1-2">
-                <input type="text" id="postcode" class="uk-input" placeholder="Postcode"/>
-            </div>
-            <div class="uk-width-1-2">
-                <button type="button" class="check-button uk-button uk-button-primary">Check</button>
-            </div>
-    </form>
+    <div uk-spinner id="sc-spinner"  style="display:none"></div>
+    <div class="bs-form-div">
+        <form class="uk-grid-small">
+                <div class="uk-width-1-2">
+                    <input type="text" id="postcode" class="uk-input" placeholder="Postcode"/>
+                </div>
+                <div class="uk-width-1-2">
+                    <button type="button" class="check-button uk-button uk-button-primary">Check</button>
+                </div>
+        </form>
+    </div>
 </div>

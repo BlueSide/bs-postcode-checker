2e860c8499b0f348591b4bd5e27da81b13a18b12
trajectcontrole <thomas@blueside.nl>
2018-06-08 16:55:27 +0200

    Textual change

--- public/partials/bs-postcode-checker-public-display.php
+++ public/partials/bs-postcode-checker-public-display.php
@@ -14,7 +14,7 @@
 ?>
 <div class="bs-postcode-form">
     <div id="pc-not-valid" style="display:none">Dit is geen geldige postcode.</div>
-    <div id="pc-not-found" style="display:none">Uw postcode is niet aangesloten aan ons warmtenet. </div>
+    <div id="pc-not-found" style="display:none">Uw postcode is niet aangesloten op ons warmtenet. </div>
     <div id="pc-error" style="display:none">Helaas, er lijkt iets niet helemaal goed te gaan in het controleren van uw postcode. U kunt eventueel contact opnemen met onze klantenservice via <a href="mailto:klantenservice@warmte-eindhoven.nl">klantenservice@warmte-eindhoven.nl</a> of <a href="tel:+31852734500">085 273 45 00</a>.</div>
     <div id="pc-found" style="display:none"></div>
     <div uk-spinner id="sc-spinner"  style="display:none"></div>
